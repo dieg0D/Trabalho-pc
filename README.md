ec9180a85ee2efc70d683cb247ee8e313a5c8798
dieg0d <diegodi05@hotmail.com>
2019-11-08 21:38:24 -0300

    finalizado

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Trabalho-pc
+
+Este repositório contém o Projeto da Disciplina Programação concorrente de 2019/2. O trabalho consiste em implementar em C um algoritmo que solucione algum problema de comunicação entre processos que utilize uma solução de memória compartilhada que envolva concorrência e condições de corrida utilizando os mecanismos de sincronização estudados em aula (locks, variáveis condição, semáforos, etc.). Utilizando a biblioteca POSIX Threads.
+
+# Para executar utilize o seguinte comando:
+
+```bash
+    reset && gcc -o main main.c -lpthread && ./main
+```
--- main.c
+++ main.c
@@ -1,8 +1,17 @@
+/**
+ *  Trabalho: RPG concorrente
+ *  Alunos: Diego Antônio Barbosa Cardoso - 16/0005116
+ *          Icaro Nery Rezende            - 15/0037023
+ *
+ *
+ */
+
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
 
 #define RESET "\x1B[0m"
@@ -15,78 +24,314 @@
 #define RED "\x1b[31m"      // Utilizada para o dragon
 #define WHITE "\x1b[37m"    // Utilizada para o skeleton
 
-#define NUMERO_DE_GUERREIROS 6
-#define NUMERO_DE_MAGOS 6
-#define NUMERO_DE_ARQUEIROS 6
-#define NUMERO_DE_ASSASSINOS 6
+#define NUMERO_DE_GUERREIROS 15
+#define NUMERO_DE_MAGOS 8
+#define NUMERO_DE_ARQUEIROS 5
+#define NUMERO_DE_ASSASSINOS 5
 
-int GOOD_NEXUS = 100;  // Vida total da party
-int EVIL_NEXUS = 100;  // vida total dos monstros
+// 5 de cada classe totaliza 200 de vida
+// NUMERO_DE_GUERREIROS * 20 + NUMERO_DE_MAGOS * 5 + NUMERO_DE_ARQUEIROS * 10 +
+// NUMERO_DE_ASSASSINOS * 5
+int GOOD_NEXUS = NUMERO_DE_GUERREIROS * 20 + NUMERO_DE_MAGOS * 5 +
+                 NUMERO_DE_ARQUEIROS * 10 +
+                 NUMERO_DE_ASSASSINOS * 5;  // Vida total da party
+int EVIL_NEXUS = 200;                       // vida total dos monstros
+
+int ta_pegando_fogo_bixo = 0;
+int ta_tremendo_tudo_bixo = 0;
+int ta_puto_bixo = 0;
 
 // Declaração de threads, mutex e semáforos
 pthread_t dragon, skeleton, giant, guerreiros[NUMERO_DE_GUERREIROS],
     magos[NUMERO_DE_MAGOS], arqueiros[NUMERO_DE_ARQUEIROS],
     assassinos[NUMERO_DE_ASSASSINOS];
-pthread_mutex_t mutexGuerreiro, mutexMago, mutexArqueiro, mutexAssassino;
-sem_t passeAM, passeAF, passeBM, passeBF, banheiroM, banheiroF;
+pthread_mutex_t mutexparty;
+sem_t ataque_dragon, ataque_giant, ataque_skeleton;
+
+int select_target() {
+  int numero_randomico;
+  srand((unsigned)time(NULL));
+  numero_randomico = (rand()) % 3;
+  return numero_randomico;
+}
 
 void print_nexus() {
-  printf(CYAN "NEXUS ALIADO  ~~> [");
+  printf(CYAN "\n\nNEXUS ALIADO  ~~> [");
   for (int i = 1; i < (GOOD_NEXUS / 10); i++) {
     printf("#");
   }
   printf("]\n");
 
   printf(RED "NEXUS INIMIGO ~~> [");
-  for (int i = 1; i < (EVIL_NEXUS / 10); i++) {
+  for (int i = 1; i < (EVIL_NEXUS / 5); i++) {
     printf("#");
   }
-  printf("]\n\n\n");
+  printf("]\n\n");
 }
 
 void *ataque_guerreiro(void *arg) {
   int i = *((int *)arg);
   while (EVIL_NEXUS > 0) {
-    print_nexus();
-    printf(YELLOW "Guerreiro %d atacou \n", i);
-    EVIL_NEXUS = EVIL_NEXUS - 1;
-    sleep(3);
+    switch (select_target()) {
+      case 0:
+        sem_wait(&ataque_dragon);
+        if (ta_pegando_fogo_bixo) {
+          pthread_mutex_lock(&mutexparty);
+          pthread_mutex_unlock(&mutexparty);
+        }
+        printf(YELLOW "O Guerreiro %d atacou o dragão\n", i);
+        EVIL_NEXUS = EVIL_NEXUS - 1;
+        sleep(2);
+        sem_post(&ataque_dragon);
+        break;
+
+      case 1:
+        sem_wait(&ataque_giant);
+        if (ta_tremendo_tudo_bixo) {
+          pthread_mutex_lock(&mutexparty);
+          pthread_mutex_unlock(&mutexparty);
+        }
+        printf(YELLOW "O Guerreiro %d atacou o Gigante\n", i);
+        EVIL_NEXUS = EVIL_NEXUS - 1;
+        sleep(2);
+        sem_post(&ataque_giant);
+        break;
+
+      case 2:
+        sem_wait(&ataque_skeleton);
+        if (ta_puto_bixo) {
+          pthread_mutex_lock(&mutexparty);
+          pthread_mutex_unlock(&mutexparty);
+        }
+        printf(YELLOW "O Guerreiro %d atacou o Esqueleto\n", i);
+        EVIL_NEXUS = EVIL_NEXUS - 1;
+        sleep(2);
+        sem_post(&ataque_skeleton);
+        break;
+
+      default:
+        break;
+    }
   }
+  printf(CYAN "Os herois venceram\n");
+  exit(0);
 }
 
 void *ataque_arqueiro(void *arg) {
   int i = *((int *)arg);
   while (EVIL_NEXUS > 0) {
-    print_nexus();
-    printf(GREEN "Arqueiro %d atacou \n", i);
-    EVIL_NEXUS = EVIL_NEXUS - 3;
-    sleep(2);
+    switch (select_target()) {
+      case 0:
+        sem_wait(&ataque_dragon);
+        if (ta_pegando_fogo_bixo) {
+          pthread_mutex_lock(&mutexparty);
+          pthread_mutex_unlock(&mutexparty);
+        }
+        printf(GREEN "O Arqueiro %d atacou o dragão\n", i);
+        EVIL_NEXUS = EVIL_NEXUS - 2;
+        sleep(1);
+        sem_post(&ataque_dragon);
+        break;
+
+      case 1:
+        sem_wait(&ataque_giant);
+        if (ta_tremendo_tudo_bixo) {
+          pthread_mutex_lock(&mutexparty);
+          pthread_mutex_unlock(&mutexparty);
+        }
+        printf(GREEN "O Arqueiro %d atacou o Gigante\n", i);
+        EVIL_NEXUS = EVIL_NEXUS - 2;
+        sleep(1);
+        sem_post(&ataque_giant);
+        break;
+
+      case 2:
+        sem_wait(&ataque_skeleton);
+        if (ta_puto_bixo) {
+          pthread_mutex_lock(&mutexparty);
+          pthread_mutex_unlock(&mutexparty);
+        }
+        printf(GREEN "O Arqueiro %d atacou o Esqueleto\n", i);
+        EVIL_NEXUS = EVIL_NEXUS - 2;
+        sleep(1);
+        sem_post(&ataque_skeleton);
+        break;
+
+      default:
+        break;
+    }
   }
+  printf(CYAN "\n\nOs herois venceram\n");
+  exit(0);
 }
 
 void *ataque_mago(void *arg) {
   int i = *((int *)arg);
   while (EVIL_NEXUS > 0) {
-    print_nexus();
-    printf(BLUE "Mago %d atacou \n", i);
-    EVIL_NEXUS = EVIL_NEXUS - 5;
-    sleep(5);
+    switch (select_target()) {
+      case 0:
+        sem_wait(&ataque_dragon);
+        if (ta_pegando_fogo_bixo) {
+          pthread_mutex_lock(&mutexparty);
+          pthread_mutex_unlock(&mutexparty);
+        }
+        printf(BLUE "O Mago %d atacou o dragão\n", i);
+        EVIL_NEXUS = EVIL_NEXUS - 5;
+        sleep(8);
+        sem_post(&ataque_dragon);
+        break;
+
+      case 1:
+        sem_wait(&ataque_giant);
+        if (ta_tremendo_tudo_bixo) {
+          pthread_mutex_lock(&mutexparty);
+          pthread_mutex_unlock(&mutexparty);
+        }
+        printf(BLUE "O Mago %d atacou o Gigante\n", i);
+        EVIL_NEXUS = EVIL_NEXUS - 5;
+        sleep(8);
+        sem_post(&ataque_giant);
+        break;
+
+      case 2:
+        sem_wait(&ataque_skeleton);
+        if (ta_puto_bixo) {
+          pthread_mutex_lock(&mutexparty);
+          pthread_mutex_unlock(&mutexparty);
+        }
+        printf(BLUE "O Mago %d atacou o Esqueleto\n", i);
+        EVIL_NEXUS = EVIL_NEXUS - 5;
+        sleep(8);
+        sem_post(&ataque_skeleton);
+        break;
+
+      default:
+        break;
+    }
   }
+  printf(CYAN "\n\nOs herois venceram\n");
+  exit(0);
 }
 
 void *ataque_assassino(void *arg) {
   int i = *((int *)arg);
   while (EVIL_NEXUS > 0) {
-    print_nexus();
-    printf(CYAN "Assassino %d atacou \n", i);
-    EVIL_NEXUS = EVIL_NEXUS - 2;
-    sleep(1);
+    switch (select_target()) {
+      case 0:
+        sem_wait(&ataque_dragon);
+        if (ta_pegando_fogo_bixo) {
+          pthread_mutex_lock(&mutexparty);
+          pthread_mutex_unlock(&mutexparty);
+        }
+        printf(CYAN "O Assassino %d atacou o dragão\n", i);
+        EVIL_NEXUS = EVIL_NEXUS - 4;
+        sleep(4);
+        sem_post(&ataque_dragon);
+        break;
+
+      case 1:
+        sem_wait(&ataque_giant);
+        if (ta_tremendo_tudo_bixo) {
+          pthread_mutex_lock(&mutexparty);
+          pthread_mutex_unlock(&mutexparty);
+        }
+        printf(CYAN "O Assassino %d atacou o Gigante\n", i);
+        EVIL_NEXUS = EVIL_NEXUS - 4;
+        sleep(4);
+        sem_post(&ataque_giant);
+        break;
+
+      case 2:
+        sem_wait(&ataque_skeleton);
+        if (ta_puto_bixo) {
+          pthread_mutex_lock(&mutexparty);
+          pthread_mutex_unlock(&mutexparty);
+        }
+        printf(CYAN "O Assassino %d atacou o Esqueleto\n", i);
+        EVIL_NEXUS = EVIL_NEXUS - 4;
+        sleep(4);
+        sem_post(&ataque_skeleton);
+        break;
+
+      default:
+        break;
+    }
   }
+  printf(CYAN "\n\nOs herois venceram\n");
+  exit(0);
+}
+
+void *ataque_especial_dragon(void *arg) {
+  while (GOOD_NEXUS > 0) {
+    sleep(15);
+    printf(RED "\n\nO Dragão se prepara para atacar \n");
+    pthread_mutex_lock(&mutexparty);
+    ta_pegando_fogo_bixo = 1;
+    printf(RED "O Dragão ta mandando fogo pra todo lado \n");
+    sleep(1);
+    GOOD_NEXUS = GOOD_NEXUS - 30;
+    printf(RED "O Dragão terminou de atacar \n");
+    print_nexus();
+    ta_pegando_fogo_bixo = 0;
+    pthread_mutex_unlock(&mutexparty);
+  }
+  printf(RED "\n\nOs Monstros Venceram\n");
+  exit(0);
+}
+
+void *ataque_especial_giant(void *arg) {
+  while (GOOD_NEXUS > 0) {
+    sleep(10);
+    printf(MAGENTA "\n\nO Gigante se prepara para atacar \n");
+    pthread_mutex_lock(&mutexparty);
+    ta_tremendo_tudo_bixo = 1;
+    printf(MAGENTA "O Gigante ta pisando em todo mundo \n");
+    sleep(1);
+    GOOD_NEXUS = GOOD_NEXUS - 15;
+    printf(MAGENTA "O Gigante terminou de atacar \n");
+    print_nexus();
+    ta_tremendo_tudo_bixo = 0;
+    pthread_mutex_unlock(&mutexparty);
+  }
+  printf(RED "\n\nOs Monstros Venceram\n");
+  exit(0);
+}
+
+void *ataque_especial_skeleton(void *arg) {
+  while (GOOD_NEXUS > 0) {
+    sleep(5);
+    printf(WHITE "\n\nO Esqueleto se prepara para atacar \n");
+    pthread_mutex_lock(&mutexparty);
+    ta_puto_bixo = 1;
+    printf(WHITE "O Esqueleto grita: 'SE EU PUDESSE EU MATAVA 1000' \n");
+    sleep(1);
+    GOOD_NEXUS = GOOD_NEXUS - 5;
+    printf(WHITE "O Esqueleto terminou de atacar \n");
+    print_nexus();
+    ta_puto_bixo = 0;
+    pthread_mutex_unlock(&mutexparty);
+  }
+  printf(RED "\n\nOs Monstros Venceram\n\n");
+  exit(0);
 }
 
 int main() {
   int i = 0;
   int *id;
+
+  printf(RED "############################################\n");
+  printf(RED "###                                      ###\n");
+  printf(RED "###    A BATALHA FINAL IRÁ COMEÇAR!!!!   ###\n");
+  printf(RED "###                                      ###\n");
+  printf(RED "############################################\n\n");
+
+  print_nexus();
+
+  sem_init(&ataque_dragon, 0, 1);
+  sem_init(&ataque_giant, 0, 1);
+  sem_init(&ataque_skeleton, 0, 1);
+
+  pthread_mutex_init(&mutexparty, NULL);
 
   // Criação das threads de guerreiros
   for (i = 0; i < NUMERO_DE_GUERREIROS; i++) {
@@ -95,42 +340,46 @@ int main() {
     pthread_create(&(guerreiros[i]), NULL, ataque_guerreiro, (void *)id);
   }
 
-  // Criação das threads de arqueiros
   for (i = 0; i < NUMERO_DE_ARQUEIROS; i++) {
     id = (int *)malloc(sizeof(int));
     *id = i;
     pthread_create(&(arqueiros[i]), NULL, ataque_arqueiro, (void *)id);
   }
 
-  // Criação das threads de magos
   for (i = 0; i < NUMERO_DE_MAGOS; i++) {
     id = (int *)malloc(sizeof(int));
     *id = i;
     pthread_create(&(magos[i]), NULL, ataque_mago, (void *)id);
   }
 
-  // Criação das threads de assassinos
   for (i = 0; i < NUMERO_DE_ASSASSINOS; i++) {
     id = (int *)malloc(sizeof(int));
     *id = i;
     pthread_create(&(assassinos[i]), NULL, ataque_assassino, (void *)id);
   }
 
+  pthread_create(&dragon, NULL, ataque_especial_dragon, NULL);
+  pthread_create(&giant, NULL, ataque_especial_giant, NULL);
+  pthread_create(&skeleton, NULL, ataque_especial_skeleton, NULL);
+
   for (i = 0; i < NUMERO_DE_GUERREIROS; i++) {
     pthread_join(guerreiros[i], NULL);
   }
 
-  for (i = 0; i < NUMERO_DE_ARQUEIROS; i++) {
+  for (i = 0; i < NUMERO_DE_GUERREIROS; i++) {
     pthread_join(arqueiros[i], NULL);
   }
 
-  for (i = 0; i < NUMERO_DE_MAGOS; i++) {
+  for (i = 0; i < NUMERO_DE_GUERREIROS; i++) {
     pthread_join(magos[i], NULL);
   }
 
-  for (i = 0; i < NUMERO_DE_ASSASSINOS; i++) {
+  for (i = 0; i < NUMERO_DE_GUERREIROS; i++) {
     pthread_join(assassinos[i], NULL);
   }
+  pthread_join(dragon, NULL);
+  pthread_join(giant, NULL);
+  pthread_join(skeleton, NULL);
 
   return 0;
 }
